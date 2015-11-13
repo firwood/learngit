78ae2dbca89c71a4e7fdfdfecb35037214dd3e4e
firwood <firwood@live.com>
2015-11-13 23:51:42 +0800

    init

---
+++ Helloworld/src/com/firwood/hello/HelloAction.java
@@ -0,0 +1,10 @@
+package com.firwood.hello;
+
+public class HelloAction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
