f9f94ab7b15df9cecaee891a15bebdcdcd9570d4
firwood <firwood@live.com>
2015-11-14 11:03:50 +0800

    message add hellogit

--- Helloworld/src/com/firwood/hello/HelloAction.java
+++ Helloworld/src/com/firwood/hello/HelloAction.java
@@ -5,6 +5,7 @@ public class HelloAction {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
+		System.out.println("HelloGit");
 	}
 
 }
